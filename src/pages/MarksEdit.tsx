106ce8b60736d2cdd537f82758aacd80de87d616
Semenov Alexey <alexpskov60@gmail.com>
2022-05-14 02:05:55 +0300

    Initial commit

---
+++ src/pages/MarksEdit.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+import MarksList from '../components/MarksList'
+import { FC } from 'react'
+import { ToastContainer, toast } from 'react-toastify'
+
+const Container = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 10px;
+`
+
+const Title = styled.h1`
+  margin: 0 0 15px 0;
+  font-size: 24px;
+`
+
+const MarksEdit: FC = () => {
+    return (
+        <Container>
+            <Title>
+                Список точек
+            </Title>
+            <MarksList />
+        </Container>
+    )
+}
+
+export default MarksEdit
---
+++ src/utils/createID.ts
@@ -0,0 +1,5 @@
+const createID = () : string => {
+    return Date.now().toString(36) + Math.random().toString(36).slice(2)
+}
+
+export default createID
